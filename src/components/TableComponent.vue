3173ef3dee325d7a16576a7453b6b59bf5705b38
AcheZeta <ache.mariana@gmail.com>
2019-12-04 13:37:30 -0600

    Config: Table

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="hello"></div>
+  <div class="hello">
+    <table>
+      <tr>
+        <th>id</th>
+        <th>UserId</th>
+        <th>Title</th>
+        <th>Body</th>
+      </tr>
+      <tr v-for="item in posts" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.userId }}</td>
+        <td>{{ item.title }}</td>
+        <td>{{ item.body }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import env from "../config/env";
+
 export default {
   name: "TableComponent",
-  props: {
-    msg: String
+  data() {
+    return {
+      posts: [],
+      postBody: null,
+      postTitle: null
+    };
+  },
+  created: async function() {
+    let postsResponse = await axios.get(`${env.endpoint}/posts/`);
+    this.posts = postsResponse.data;
   }
 };
 </script>
