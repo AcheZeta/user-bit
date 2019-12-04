601255541a08aeaef962cd0b2af1e63512625679
AcheZeta <ache.mariana@gmail.com>
2019-12-04 13:40:48 -0600

    Config

---
+++ src/components/formComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="text" v-model="postTitle" placeholder="title" />
+    <input type="text" v-model="postBody" placeholder="body" />
+    <input type="button" @click="createPost()" value="submit" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inputForm"
+};
+</script>
+
+<style lang="scss" scoped></style>
